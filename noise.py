cf84c90f73dfdeddbfa40adb753c17e17b03f57e
Tod Kurt <todbot@users.noreply.github.com>
2023-02-06 15:08:12 -0800

    noise.py passed pylint and black

--- noise.py
+++ noise.py
@@ -1,3 +1,4 @@
+# pylint: disable=invalid-name
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2023 Tod Kurt
 #
@@ -6,7 +7,7 @@
 `noise`
 ================================================================================
 
-Simplex noise (like Perlin) for CircuitPython
+Simplex noise (like Perlin) for CircuitPython.
 
 
 * Author(s): Tod Kurt
@@ -20,6 +21,7 @@ No special requirements. Also works in desktop CPython.
 
 """
 
+
 # imports
 
 __version__ = "0.0.0+auto.0"
@@ -28,12 +30,18 @@ __repo__ = "https://github.com/todbot/CircuitPython_Noise.git"
 
 from math import sqrt, floor
 
-class Grad:
-    def __init__(self, x, y, z):
-        self.x,self.y,self.z = x,y,z
 
-F2 = 0.5 * (sqrt( 3.0 ) - 1.0)
-G2 = (3.0 - sqrt( 3.0 )) / 6.0
+class Grad:  # pylint: disable=too-few-public-methods
+    """Holder for 3D grad"""
+
+    def __init__(self, x, y, z):
+        self.x, self.y, self.z = x, y, z
+
+
+F2 = 0.5 * (sqrt(3.0) - 1.0)
+G2 = (3.0 - sqrt(3.0)) / 6.0
+
+# fmt: off
 p = (  # 256 elements
     151,160,137,91,90,15,131,13,201,95,96,53,194,233,7,225,140,36,103,30,69,
     142,8,99,37,240,21,10,23,190,6,148,247,120,234,75,0,26,197,62,94,252,219,
@@ -54,30 +62,43 @@ grad3 = (
     Grad(-1,0,1),Grad(1,0,-1),Grad(-1,0,-1),Grad(0,1,1),Grad(0,-1,1),
     Grad(0,1,-1),Grad(0,-1,-1)
 )
+# fmt: on
 
 perm = [0] * 512  # filled out in noise_init()
 permMod12 = [0] * 512
 
-def dot( grad, x, y ):
+
+def dot(grad, x, y):
+    """Compute dot product of grad against x,y"""
     return grad.x * x + grad.y * y
 
+
 def noise_init():
-    # Initialize permutation arrays
+    """Initialize permutation arrays. Done automatically on module load"""
     for i in range(512):
         perm[i] = p[i & 255]
-        permMod12[i] = perm[i] % 12;
+        permMod12[i] = perm[i] % 12
 
-def noise(xin, yin=0):
-    # 2D SimplexNoise noise
+
+def noise(xin, yin=0):  # pylint: disable=too-many-locals
+    """2D SimplexNoise
+    :param float xin x-location in 2D noise space
+    :param float yin y-location in 2D noise space
+
+    :return float noise value between -1 and 1
+    """
+
     s = (xin + yin) * F2
-    i = floor( xin + s )
-    j = floor( yin + s )
+    i = floor(xin + s)
+    j = floor(yin + s)
     t = (i + j) * G2
     x0 = xin - (i - t)
     y0 = yin - (j - t)
-    i1 = 0; j1 = 1
+    i1 = 0
+    j1 = 1
     if x0 > y0:
-        i1 = 1;  j1 = 0
+        i1 = 1
+        j1 = 0
 
     x1 = x0 - i1 + G2
     y1 = y0 - j1 + G2
@@ -88,22 +109,23 @@ def noise(xin, yin=0):
     gi0 = permMod12[ii + perm[jj]]
     gi1 = permMod12[ii + i1 + perm[jj + j1]]
     gi2 = permMod12[ii + 1 + perm[jj + 1]]
-    n0 = 0.0;
+    n0 = 0.0
     t0 = 0.5 - x0 * x0 - y0 * y0
-    if t0 >= 0.0 :
+    if t0 >= 0.0:
         t0 *= t0
-        n0 = t0 * t0 * dot( grad3[gi0], x0, y0 )
+        n0 = t0 * t0 * dot(grad3[gi0], x0, y0)
     n1 = 0.0
     t1 = 0.5 - x1 * x1 - y1 * y1
     if t1 >= 0.0:
         t1 *= t1
-        n1 = t1 * t1 * dot( grad3[gi1], x1, y1 )
+        n1 = t1 * t1 * dot(grad3[gi1], x1, y1)
     n2 = 0.0
     t2 = 0.5 - x2 * x2 - y2 * y2
     if t2 >= 0.0:
         t2 *= t2
-        n2 = t2 * t2 * dot( grad3[gi2], x2, y2 )
+        n2 = t2 * t2 * dot(grad3[gi2], x2, y2)
 
-    return 70.0 * (n0 + n1 + n2);
+    return 70.0 * (n0 + n1 + n2)
+
 
 noise_init()
